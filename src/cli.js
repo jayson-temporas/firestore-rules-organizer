7a14629ec998712c19877eeebee16db5e2c3161e
jayson-temporas <jayson.temporas@gmail.com>
2021-06-22 12:04:20 +0800

    add log message

--- src/cli.js
+++ src/cli.js
@@ -7,6 +7,7 @@ const fs = require('fs');
 var fileNames = [ __dirname + '/includes/head.rules'];
 
 fs.readdirSync(rulesFolder).forEach(file => {
+    console.log('\x1b[32m', "Added Firestore Rules: " + file, '\x1b[0m')
     fileNames.push(rulesFolder + file);
 });
 
